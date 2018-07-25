8e78d1040a6e2bae1577c484feefb2b92038ab9d
Kefu Chai <tchaikov@gmail.com>
2018-07-25 19:49:38 +0800

    cmake: link against libsnappy.a if WITH_SNAPPY_STATIC_LIB
    Signed-off-by: Kefu Chai <tchaikov@gmail.com>

--- cmake/modules/Findsnappy.cmake
+++ cmake/modules/Findsnappy.cmake
@@ -5,13 +5,29 @@
 # SNAPPY_LIBRARIES - List of libraries when using snappy.
 # SNAPPY_FOUND - True if snappy found.
 
+find_package(PkgConfig)
+pkg_search_module(PC_snappy
+  QUIET snappy)
+
 find_path(SNAPPY_INCLUDE_DIR
   NAMES snappy.h
-  HINTS ${SNAPPY_ROOT_DIR}/include)
+  HINTS
+    ${PC_snappy_INCLUDE_DIRS}
+    ${SNAPPY_ROOT_DIR}/include)
 
+
+if(SNAPPY_USE_STATIC_LIB)
+  set(_snappy_orig_FIND_LIBRARY_SUFFIXES ${CMAKE_FIND_LIBRARY_SUFFIXES})
+  set(CMAKE_FIND_LIBRARY_SUFFIXES ${CMAKE_STATIC_LIBRARY_SUFFIX})
+endif()
 find_library(SNAPPY_LIBRARIES
   NAMES snappy
-  HINTS ${SNAPPY_ROOT_DIR}/lib)
+  HINTS
+    ${PC_snappy_LIBRARY_DIRS}
+    ${SNAPPY_ROOT_DIR}/lib)
+if(SNAPPY_USE_STATIC_LIB)
+  set(CMAKE_FIND_LIBRARY_SUFFIXES ${_snappy_orig_FIND_LIBRARY_SUFFIXES})
+endif()
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(snappy DEFAULT_MSG SNAPPY_LIBRARIES SNAPPY_INCLUDE_DIR)
